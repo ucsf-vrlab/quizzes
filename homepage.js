30208f1b075fcd42c2d087873be708357d8c2e16
ucsf-vrlab <lila.lin@ucsf.edu>
2025-09-23 23:42:14 -0700

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -162,7 +162,7 @@ document.getElementById("toggle-annotations").addEventListener("click", () => {
     // back to quiz mode
     emptyAnnotations(api);
     document.getElementById("toggle-annotations").textContent =
-      "Show Original Annotations";
+      "Hide Model Annotations";
   } else {
     // restore originals
     originalAnnotations.forEach((annot, i) => {
